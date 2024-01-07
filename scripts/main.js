ca426d2fa0e88d60313d6e1c69b30b1bb5f21312
xudeyu444 <152866414+xudeyu444@users.noreply.github.com>
2024-01-07 14:59:49 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,14 +16,14 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem("name", myName);
-      myHeading.textContent = "Mozilla 酷毙了，" + myName;
+      myHeading.textContent = "亲爱的同志" + myName + "欢迎";
     }
   }
   if (!localStorage.getItem("name")) {
     setUserName();
   } else {
     let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+    myHeading.textContent = "亲爱的同志" + storedName + "欢迎";
   }
   myButton.onclick = function () {
     setUserName();
